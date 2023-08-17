f104817cd98f9ebc312e128e8551bac0d47355d4
HridaySarder <hridoyaug12@gmail.com>
2023-08-17 21:58:37 +0600

    add another area

--- js/calculator.js
+++ js/calculator.js
@@ -19,13 +19,29 @@ function calculatorRectangleArea(){
   const widthField = document.getElementById('rectangle-width');
   const widthValueText = widthField.value;
   const width = parseFloat(widthValueText);
-  console.log(width);
+  
   const lengthField = document.getElementById('rectangle-length');
   const lengthValueText = lengthField.value;
   const length = parseFloat(lengthValueText);
-  console.log(length);
+  
 
   const area = width * length;
   const rectangleArea = document.getElementById('rectangle-area');
   rectangleArea.innerText = area;
+}
+
+function calculatorParallelogramArea(){
+  const baseField = document.getElementById('parallelogram-base');
+const baseValueText = baseField.value;
+const base = parseFloat(baseValueText);
+
+const heightField = document.getElementById('parallelogram-height');
+const heightValueText = heightField.value;
+const height = parseFloat(heightValueText);
+
+
+const area = base * height;
+const parallelogramArea = document.getElementById('parallelogram-area');
+parallelogramArea.innerText = area;
+
 }
